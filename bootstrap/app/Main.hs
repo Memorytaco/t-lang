8362ec355d755a6c89b74734c6a5aa046f612002
dunor <dunor@dunor.dev>
2022-10-31 00:26:25 +0800

    Add basic commandline options

--- bootstrap/app/Main.hs
+++ bootstrap/app/Main.hs
@@ -2,6 +2,9 @@ module Main (main) where
 
 import Tlang
 
+import CLI.Parser (CommandLineOption (..), command)
+
+import Control.Monad
 import Control.Monad.Trans
 import System.Console.Haskeline
 import qualified LLVM.AST as AST
@@ -33,18 +36,34 @@ repl = runInputT defaultSettings (loop (createModule "stdin"))
                 mod'maybe <- liftIO $ process transmod input
                 loop (maybe transmod id mod'maybe)
 
+helpMsg = [ "tool <command> {args}"
+          , "commands"
+          , "  help,--help,-h"
+          , "  repl"
+          , "  compile <input path> {output}"
+          ]
+
+printHelp = mapM_ putStrLn helpMsg
+
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [] -> repl
-    [ifile] -> readFile ifile >>= process (createModule ifile) >> return ()
-    [ifile, ofile] -> readFile ifile >>= process (createModule ifile) >>= \mod'maybe -> do
-      case mod'maybe of
-        Nothing -> return ()
-        Just transmod -> withContext \context -> do
-          withModuleFromAST context transmod \llvmMod -> do
-            initializeNativeTarget
-            withHostTargetMachineDefault \targetMachine -> do
-              writeObjectToFile targetMachine (File $ ofile ++ ".o") llvmMod
+    [] -> printHelp
+    _ -> case command args of
+     Left err -> putStrLn $ show err
+     Right cmd -> do
+       putStrLn $ show cmd
+       case cmd of
+          REPL -> repl
+          HELP _ -> printHelp
+          COMPILE ifile Nothing -> readFile ifile >>= process (createModule ifile) >> return ()
+          COMPILE ifile (Just ofile) -> readFile ifile >>= process (createModule ifile) >>= \mod'maybe -> do
+            case mod'maybe of
+              Nothing -> return ()
+              Just transmod -> withContext \context -> do
+                withModuleFromAST context transmod \llvmMod -> do
+                  initializeNativeTarget
+                  withHostTargetMachineDefault \targetMachine -> do
+                    writeObjectToFile targetMachine (File $ ofile ++ ".o") llvmMod
 
---
+++ bootstrap/src/CLI/Parser.hs
@@ -0,0 +1,43 @@
+module CLI.Parser
+  ( CommandLineOption (..)
+  , command
+  )
+where
+
+import Text.Parsec
+import Text.Parsec.String
+
+data CommandLineOption =
+    REPL
+  | HELP (Maybe String)
+  | COMPILE String (Maybe String)
+  deriving (Show, Eq)
+
+help :: Parser CommandLineOption
+help = do
+  _ <- string "help" <|> try (string "--help") <|> string "-h"
+  spaces
+  topic <- optionMaybe (try $ many1 anyChar)
+  return $ HELP topic
+
+repl :: Parser CommandLineOption
+repl = do
+  _ <- string "repl"
+  return REPL
+
+identifier :: Parser String
+identifier = do
+  let symbol = alphaNum <|> char '.' <|> char '_' <|> char '-'
+  many1 symbol
+
+compile :: Parser CommandLineOption
+compile = do
+  string "compile" >> spaces
+  outs <- sepBy1 identifier spaces
+  return $ case outs of
+    [a] -> COMPILE a Nothing
+    [a, b] -> COMPILE a (Just b)
+    (a:b:_) -> COMPILE a (Just b)
+
+command :: [String] -> Either ParseError CommandLineOption
+command ags = parse (try help <|> try repl <|> compile) "commandline" $ foldl1 (\a b -> a <> " " <> b) ags
